545ef229ed328bb4ea3382208ac3ca4459906975
perchik7777 <124269112+perchik7777@users.noreply.github.com>
2023-08-25 12:34:13 +0300

    config.py

---
+++ MoneyBot/config.py
@@ -0,0 +1,13 @@
+TOKEN = '6187895738:AAGiqwhliuxkkCAgwaJp1tNcEkoX3jYHWBM'
+
+keys = {'биткоин': 'BTC',
+    'эфириум': 'ETC',
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'рубль': 'RUB',
+    'каспа': 'KAS',
+    'юсдт': 'USDT',
+    'KASPA': 'KAS',
+    'bitcoin': 'BTC',
+    'тезер': 'USDT',
+        }

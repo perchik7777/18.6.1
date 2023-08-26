7ee04cd136da50b255afe0396786da8ea5dedc4c
perchik7777 <124269112+perchik7777@users.noreply.github.com>
2023-08-26 13:09:20 +0300

    extensions.py

--- MoneyBot/extensions.py
+++ MoneyBot/extensions.py
@@ -1,36 +1,39 @@
 import requests
 import json
 from config import keys
-
-
+from config import TOKEN
 
 class ConvertionException(Exception):
     pass
+
 
 class CryptoConverter:
     @staticmethod
     def get_price(quote: str, base: str, amount: str):
         if quote == base:
             raise ConvertionException(f'Невозможно перевести одинаковые валюты {base}.')
-
+            text = f'<Невозможно перевести одинаковые валюты> '
+            bot.send_message(message.chat.id, text)
         try:
             quote_ticker = keys[quote]
         except KeyError:
             raise ConvertionException(f'Не удалось обработать валюту {quote}')
-            #
-
-
+            text = '\n <Не удалось обработать переводимую валюту> '
+            bot.send_message(message.chat.id, text)
         try:
             base_ticker = keys[base]
         except KeyError:
-            raise ConvertionException(f'Не удалось обработать валюту {base} ')
-            #
+            raise ConvertionException(f'Не удалось обработать валюту {base}')
+            text = '\n <Не удалось обработать валюту>'
+            bot.send_message(message.chat.id, text)
         try:
             amount = float(amount)
         except ValueError:
             raise ConvertionException(f'Не удалось обработать количество {amount}')
-            #
-        r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
-        total_base = amount * json.loads(r.content)[keys[base]]
+            text = '\n Не удалось обработать количество'
+            bot.send_message(message.chat.id, text)
+        r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}').json()
 
-        return total_base
+        return r[base_ticker]
+
+
